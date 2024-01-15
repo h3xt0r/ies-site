de824e04a73aec9a7f8b4f5b50a6a711b3f395aa
Hector Hernandez <hhcbrg@gmail.com>
2024-01-15 16:22:21 -0600

    Inicio del sitio IES

---
+++ assets/js/ncl.js
@@ -0,0 +1,8 @@
+/*
+	Ingeniería Esencial S.A. de C.V.
+	IES Mex | @nekhya
+	*/
+document.addEventListener("contextmenu", function(event) {
+  event.preventDefault();
+  alert("No está permitido hacer click derecho en esta página. No lo intentes, la maldicón de Cthulhu caerá en ti");
+});
